96ff761768b6c301125b67081e1c05a3d9e87961
sujivennapusa <vennapusasuji463@gmail.com>
2021-07-22 20:02:00 +0530

    sum

---
+++ filter.py
@@ -0,0 +1,3 @@
+a=[10,20,56,89]
+b=[56,89,30,40]
+print(list(filter(lambda x:x in a,b)))  #intersection of both lists
--- sum.py
+++ sum.py
@@ -1,6 +1,7 @@
+
+def addNum(x,y):
+    return x+y
 a=int(input("enter the number 1"))
 b=int(input("enter the second number"))
-def addNum(x,y)
-    z=x+y
-    return z
+print(addNum(a,b))    
     
