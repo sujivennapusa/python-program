f6f72343b2798eb86821c6e08a7395e21889fd7b
sujivennapusa <vennapusasuji463@gmail.com>
2021-07-22 13:16:31 +0530

    squaring

---
+++ functionref.py
@@ -0,0 +1,5 @@
+num=[]
+def appendToList(x):
+ x.append(1)
+ return x
+print(appendToList(num))
---
+++ squareroot.py
@@ -0,0 +1,12 @@
+""" n=int(input("enter the number"))
+def sqr(a): 
+   return a**2
+print(sqr(n))   """ 
+n1=int(input("enter the number:"))
+n2=int(input("enter the number:"))
+n3=int(input("enter rhe number:"))
+list1=[n1,n2,n3]
+def sqr(a):
+  return a**2
+x=list(map(sqr,list1))  
+print(x)
