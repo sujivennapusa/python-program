0350427311fc117271f45bd0c4155985a6c416dd
sujivennapusa <vennapusasuji463@gmail.com>
2021-08-01 14:10:02 +0530

    adding students using class

--- day6task/department.py
+++ day6task/department.py
@@ -1,2 +1,20 @@
 class department:
-    def __init__(self,name,rollno,)
+    def __init__(self,dept):
+        self.dept=dept
+    def addStudent(self):
+        self.name=input("enter name:")
+        self.rollno=int(input("enter rollno:"))  
+        self.admno=int(input("enter admno:"))
+        self.addr=input("enter addr:")
+        self.clz=input("enter clz:") 
+        self.mbno=int(input("enter number:"))  
+        print(self.name)
+        print(self.rollno)
+        print(self.admno)
+        print(self.addr)
+        print(self.clz)
+        print(self.mbno) 
+dept1=department("cse")
+dept1.addStudent()
+dept2=department("ece")
+dept2.addStudent()
