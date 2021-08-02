835d8df1390b715da4f430567af1b693440cb69d
sujivennapusa <vennapusasuji463@gmail.com>
2021-08-02 10:47:39 +0530

    collections

---
+++ day7tasks/multiinherit.py
@@ -0,0 +1,19 @@
+class AdditionOperation:
+    def addTwoNum(self,a,b):
+        return a+b
+class SubtractionOperation:
+    def subTwoNum(self,a,b):
+        return a-b
+class MultiplicationOperation:
+    def mulTwoNum(self,a,b):
+        return a*b        
+class Calculator(AdditionOperation,SubtractionOperation,MultiplicationOperation):
+    pass
+obj=Calculator()
+""" n1=int(input("enter the number:"))
+n2=int(input("enter the number:"))
+print(obj.addTwoNum(n1,n2))
+print(obj.subTwoNum(n1,n2))
+print(obj.mulTwoNum(n1,n2)) """
+print(issubclass(Calculator,MultiplicationOperation))
+print(issubclass(SubtractionOperation,MultiplicationOperation))
---
+++ w3day1tasks/clct.py
@@ -0,0 +1,19 @@
+""" #namedtuple()
+import collections
+employees=collections.namedtuple('employees',["name","empid","salary"])
+e1=employees("raju","10001","25000")
+print(e1[0])
+ """
+""" #OrderedDict()
+import collections
+d1=collections.OrderedDict()
+d1['name']="sony"
+d1['rollno']="22"
+d1['admno']="1001"
+
+for key,value in d1.items():
+    print(key,value) """
+#Counter()
+import collections
+x=collections.Counter(["hello","hai","hello"])
+print(x)
