890e61175bd5129b35fc1fa1e48fdb0ac6646729
sujivennapusa <vennapusasuji463@gmail.com>
2021-08-03 00:02:44 +0530

    menudriven

---
+++ day7tasks/menu.py
@@ -0,0 +1,14 @@
+while(True):
+    print("select an option from menu")
+    print("\n")
+    print("1.addition")
+    print("2.subtraction")
+    print("3.exit")
+    choice=int(input("enter the choice :"))
+    if choice==1:
+       print("addition selected")
+    if choice==2:
+       print("subtraction selected")
+    if choice==3:
+       break      
+
---
+++ day7tasks/whileloop.py
@@ -0,0 +1,12 @@
+i=int(input("enter the number:"))
+e=int(input("enter the number to end:"))
+while(i<e):
+    #print(i)
+    print(i,end=" ")
+    i=i+1
+
+
+""" i=1
+while(i<=10):
+    print(i)
+    i=i+1 """
